e38e89304b4758868a0fa38d071e080bfe928253
NamanSemalti <namansemalti@gmail.com>
2024-03-28 11:28:21 +0530

    Save&Load Added

---
+++ Assets/Scripts/Managers/SaveSystem.cs
@@ -0,0 +1,42 @@
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public static class SaveSystem
+{
+    public static void Save<T>(string fileName, T data)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/" + fileName;
+        FileStream stream = new FileStream(path, FileMode.Create);
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+    
+    public static T Load<T>(string fileName)
+    {
+        T data;
+        string path = Application.persistentDataPath + "/" + fileName;
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+            data = (T)formatter.Deserialize(stream);
+            stream.Close();
+        }
+        else
+        {
+            data = default(T);
+        }
+        return data;
+    }
+
+    public static void Delete(string fileName)
+    {
+        string path = Application.persistentDataPath + "/" + fileName;
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+        }
+    }
+}
