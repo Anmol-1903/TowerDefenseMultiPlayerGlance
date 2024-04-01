59e22e75c4d7586a7d15a8b1fa9acbd4937b11bc
NamanSemalti <namansemalti@gmail.com>
2024-04-01 09:27:52 +0530

    Added Functionality to Create Path

--- Assets/Scripts/Core/Input/InputManager.cs
+++ Assets/Scripts/Core/Input/InputManager.cs
@@ -4,13 +4,14 @@ using Core.PathHandler;
 using InputOwner = Core.GameEnums.OwnershipType;
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 using Util;
+using Tower;
 
 namespace Core.Input
 {
     public class InputManager : MonoBehaviour
     {
         [SerializeField] private InputOwner owner;
-
+        private TowerBase towerBase;
         private void OnEnable()
         {
             /*            GameManager.Instance.OnGameStart += EnableInputs;
@@ -44,14 +45,40 @@ namespace Core.Input
 
         private void Touch_onFingerDown(Finger finger)
         {
+            RaycastHit hit;
+            if (RaycastFromFinger(finger, out hit))
+            {
+                // Check if the collider belongs to a GameObject with a TowerBase script
+                towerBase = hit.collider.GetComponent<TowerBase>();
+                if (towerBase != null && towerBase.CanCreateConnections)
+                {
+                    Debug.Log("Finger touched a tower!");
+                    PathManager.Instance.GetHintLine(towerBase.transform.position);
+
+                }
+            }
         }
 
         private void Touch_onFingerMove(Finger finger)
         {
+            if (towerBase != null)
+            {
+                PathManager.Instance.UpdateHintLine(Camera.main.ScreenToWorldPoint(finger.screenPosition), true);
+            }
         }
 
         private void Touch_onFingerUp(Finger finger)
         {
+            if (towerBase != null)
+            {
+                PathManager.Instance.RemoveHintLine();
+            }
+        }
+
+        public static bool RaycastFromFinger(Finger finger, out RaycastHit hit)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(finger.screenPosition);
+            return Physics.Raycast(ray, out hit);
         }
     }
 }
