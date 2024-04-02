db175dd9842737290eee12858f7c2e8707857b8a
fallenblood7080 <67326508+fallenblood7080@users.noreply.github.com>
2024-04-02 23:18:03 +0530

    added namesapce

--- Assets/Scripts/AI/EnemyManager.cs
+++ Assets/Scripts/AI/EnemyManager.cs
@@ -7,137 +7,140 @@ using UnityEngine;
 using TowerOwner = Core.GameEnums.OwnershipType;
 using TowerType = Core.GameEnums.TowerType;
 
-public class EnemyManager : MonoBehaviour
+namespace AI
 {
-    [SerializeField] private float yOffset;
-
-    [Space(5f)]
-    [SerializeField] private TowerOwner owner;
-
-    [SerializeField] private float attackInterval;
-    [SerializeField] private LayerMask towerLayer;
-    [SerializeField, LabelByChild("PreyOwner")] private Priority[] enemyPriorities;
-
-    private List<TowerBase> selfTower;
-
-    private float nextTimeToAttack;
-    private bool canStart;
-
-    public float AttackInterval => attackInterval;
-    public Priority[] Priorities => enemyPriorities;
-
-    public bool SetEnemyData(TowerOwner type, float interval, Priority[] priorities)
+    public class EnemyManager : MonoBehaviour
     {
-        owner = type;
-        attackInterval = interval;
-        enemyPriorities = priorities;
+        [SerializeField] private float yOffset;
 
-        gameObject.name = $"{type}";
-        return true;
-    }
+        [Space(5f)]
+        [SerializeField] private TowerOwner owner;
 
-    public void InitAI()
-    {
-        /*        selfTower = new();
-                TowerTracker.Instance.OnTowerUpdate += GetSelfTower;
-                canStart = true;
-                nextTimeToAttack = attackInterval;*/
-    }
+        [SerializeField] private float attackInterval;
+        [SerializeField] private LayerMask towerLayer;
+        [SerializeField, LabelByChild("PreyOwner")] private Priority[] enemyPriorities;
 
-    private void Update()
-    {
-        if (canStart)
+        private List<TowerBase> selfTower;
+
+        private float nextTimeToAttack;
+        private bool canStart;
+
+        public float AttackInterval => attackInterval;
+        public Priority[] Priorities => enemyPriorities;
+
+        public bool SetEnemyData(TowerOwner type, float interval, Priority[] priorities)
         {
-            if (nextTimeToAttack <= 0)
+            owner = type;
+            attackInterval = interval;
+            enemyPriorities = priorities;
+
+            gameObject.name = $"{type}";
+            return true;
+        }
+
+        public void InitAI()
+        {
+            /*        selfTower = new();
+                    TowerTracker.Instance.OnTowerUpdate += GetSelfTower;
+                    canStart = true;
+                    nextTimeToAttack = attackInterval;*/
+        }
+
+        private void Update()
+        {
+            if (canStart)
             {
-                nextTimeToAttack = attackInterval;
-                Attack();
+                if (nextTimeToAttack <= 0)
+                {
+                    nextTimeToAttack = attackInterval;
+                    Attack();
+                }
+                else
+                {
+                    nextTimeToAttack -= Time.deltaTime;
+                }
             }
             else
             {
-                nextTimeToAttack -= Time.deltaTime;
+                nextTimeToAttack = attackInterval;
             }
         }
-        else
+
+        private void OnDestroy()
         {
-            nextTimeToAttack = attackInterval;
+            /*        TowerTracker.Instance.OnTowerUpdate -= GetSelfTower;
+                    GameManager.Instance.OnGameStart -= InitAI;*/
         }
-    }
 
-    private void OnDestroy()
-    {
-        /*        TowerTracker.Instance.OnTowerUpdate -= GetSelfTower;
-                GameManager.Instance.OnGameStart -= InitAI;*/
-    }
-
-    private void Attack()
-    {
-        foreach (var priority in enemyPriorities)
+        private void Attack()
         {
-            for (int i = 0; i < selfTower.Count; i++)
+            foreach (var priority in enemyPriorities)
             {
-                if (!selfTower[i].CanCreateConnections)
-                    continue;
-
-                TowerBase[] possibleTower = GetPossibleTowersToAttack(priority);
-                foreach (var tower in possibleTower)
+                for (int i = 0; i < selfTower.Count; i++)
                 {
-                    Vector3 startpoint = new(selfTower[i].transform.position.x, yOffset, selfTower[i].transform.position.z);
-                    Vector3 endpoint = new(tower.transform.position.x, yOffset, tower.transform.position.z);
-                    Vector3 dir = endpoint - startpoint;
+                    if (!selfTower[i].CanCreateConnections)
+                        continue;
 
-                    if (TryToConnect(startpoint, dir, priority, selfTower[i])) //! Try to connect with prey and if conected exit the method
+                    TowerBase[] possibleTower = GetPossibleTowersToAttack(priority);
+                    foreach (var tower in possibleTower)
                     {
-                        return;
-                    }
-                }
-            }
-        }
-    }
+                        Vector3 startpoint = new(selfTower[i].transform.position.x, yOffset, selfTower[i].transform.position.z);
+                        Vector3 endpoint = new(tower.transform.position.x, yOffset, tower.transform.position.z);
+                        Vector3 dir = endpoint - startpoint;
 
-    private bool TryToConnect(Vector3 startpoint, Vector3 dir, Priority priority, TowerBase selfTower)
-    {
-        if (Physics.Raycast(startpoint, dir.normalized, out RaycastHit hit, dir.magnitude, towerLayer))
-        {
-            if (hit.collider.CompareTag("Tower"))
-            {
-                if (hit.collider.TryGetComponent<TowerBase>(out var preyTower))
-                {
-                    if (priority.FilterByLevel && selfTower.Level < preyTower.Level)
-                    {
-                        return false;
-                    }
-
-                    if (selfTower.ConnectTo(preyTower) != null)//! if successfully connected return true
-                    {
-                        return true;
+                        if (TryToConnect(startpoint, dir, priority, selfTower[i])) //! Try to connect with prey and if conected exit the method
+                        {
+                            return;
+                        }
                     }
                 }
             }
         }
 
-        return false;
+        private bool TryToConnect(Vector3 startpoint, Vector3 dir, Priority priority, TowerBase selfTower)
+        {
+            if (Physics.Raycast(startpoint, dir.normalized, out RaycastHit hit, dir.magnitude, towerLayer))
+            {
+                if (hit.collider.CompareTag("Tower"))
+                {
+                    if (hit.collider.TryGetComponent<TowerBase>(out var preyTower))
+                    {
+                        if (priority.FilterByLevel && selfTower.Level < preyTower.Level)
+                        {
+                            return false;
+                        }
+
+                        if (selfTower.ConnectTo(preyTower) != null)//! if successfully connected return true
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private TowerBase[] GetPossibleTowersToAttack(Priority priority)
+        {
+            TowerBase[] preyByOwner, preyByType;
+            preyByOwner = TowerTracker.Instance.GetTowerByOwner(priority.PreyOwner).Except(selfTower).ToArray();
+            preyByType = TowerTracker.Instance.GetTowerByType(priority.PreyTowerType).ToArray();
+
+            return preyByOwner.Intersect(preyByType).ToArray();
+        }
+
+        private void GetSelfTower()
+        {
+            //Get All his tower
+        }
     }
 
-    private TowerBase[] GetPossibleTowersToAttack(Priority priority)
+    [Serializable]
+    public struct Priority
     {
-        TowerBase[] preyByOwner, preyByType;
-        preyByOwner = TowerTracker.Instance.GetTowerByOwner(priority.PreyOwner).Except(selfTower).ToArray();
-        preyByType = TowerTracker.Instance.GetTowerByType(priority.PreyTowerType).ToArray();
-
-        return preyByOwner.Intersect(preyByType).ToArray();
+        public bool FilterByLevel;
+        public TowerType PreyTowerType;
+        public TowerOwner PreyOwner;
     }
-
-    private void GetSelfTower()
-    {
-        //Get All his tower
-    }
-}
-
-[Serializable]
-public struct Priority
-{
-    public bool FilterByLevel;
-    public TowerType PreyTowerType;
-    public TowerOwner PreyOwner;
 }
