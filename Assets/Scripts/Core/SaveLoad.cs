a2f88e9f562408767b66abf7059cedec32f3e7c9
fallenblood7080 <67326508+fallenblood7080@users.noreply.github.com>
2024-04-02 22:34:46 +0530

    moved save file

---
+++ Assets/Scripts/Core/SaveLoad.cs
@@ -0,0 +1,69 @@
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEditor;
+using UnityEngine;
+using Util;
+
+namespace Core.SaveLoadSystem
+{
+    public static class EncryptionManager
+    {
+        private const string _fileName = ".insanity";
+
+        public static void Save<T>(T objectToSave, string key)
+        {
+            string path = Application.persistentDataPath + "/Saved Gamedata/";
+            Directory.CreateDirectory(path);
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using FileStream fileStream = new(path + key + _fileName, FileMode.Create);
+            formatter.Serialize(fileStream, objectToSave);
+        }
+
+        public static T Load<T>(string key, T defaultVal)
+        {
+            string path = Application.persistentDataPath + "/Saved Gamedata/";
+            BinaryFormatter formatter = new();
+            T returnValue = default;
+            string filePath = path + key + _fileName;
+
+            if (File.Exists(filePath))
+            {
+                using FileStream fileStream = new(filePath, FileMode.Open);
+                returnValue = (T)formatter.Deserialize(fileStream);
+            }
+            else
+            {
+                returnValue = defaultVal;
+            }
+
+            return returnValue;
+        }
+
+        public static bool HaveData(string key)
+        {
+            string path = Application.persistentDataPath + "/Saved Gamedata/" + key + _fileName;
+            return File.Exists(path);
+        }
+
+        public static void DeleteKey(string key)
+        {
+            string path = Application.persistentDataPath + "/Saved Gamedata/" + key + _fileName;
+            File.Delete(path);
+        }
+
+#if UNITY_EDITOR
+
+        [MenuItem("GameData/DeleteGameData")]
+        public static void PermanentlyDeleteGameData()
+        {
+            string path = Application.persistentDataPath + "/Saved Gamedata/";
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            directoryInfo.Delete(true);
+            Directory.CreateDirectory(path);
+            "GameData deleted".Log();
+        }
+
+#endif
+    }
+}
