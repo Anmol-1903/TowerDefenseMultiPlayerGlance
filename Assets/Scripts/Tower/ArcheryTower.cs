7ee747088f9b6af2c54ece546c850cf24e200558
Kavyansh Barjattya <117171893+kavyanshbarjattya@users.noreply.github.com>
2024-04-03 21:27:41 +0530

    Projectile

--- Assets/Scripts/Tower/ArcheryTower.cs
+++ Assets/Scripts/Tower/ArcheryTower.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
+using System.Collections;
+using Util;
 using TowerType = Core.GameEnums.TowerType;
+using OwnerShipType = Core.GameEnums.OwnershipType;
+using UnityEngine.Rendering;
+using Core;
+using Troop;
 
 namespace Tower
 {
     public class ArcheryTower : TowerBase
     {
-        [SerializeField] private float attackInterval;
+        [SerializeField] private float attackInterval, _attackingRange;
+        [SerializeField] float _InitialVel, _Angle;
+        [SerializeField] GameObject _test;
+        [SerializeField] LayerMask _troopsLayer;
+        [SerializeField] LeanTweenType _leanTweenType;
         private float currentAttackInterval;
 
         protected override void Awake()
@@ -18,6 +28,9 @@ namespace Tower
 
         protected override void Update()
         {
+            {
+                
+            }
             base.Update();
             if (currentAttackInterval < 0)
             {
@@ -38,6 +51,28 @@ namespace Tower
         protected override void Spawn()
         {
             //spawn attack/projectile
+            Collider[] troopCol = Physics.OverlapSphere(transform.position, _attackingRange , _troopsLayer);
+            foreach (Collider col in troopCol)
+            {
+                if (col.gameObject.GetComponent<Collider>().TryGetComponent<TroopBase>(out TroopBase otherOwner))
+                {
+                    if (TowerOwner != otherOwner.Owner)
+                    {
+                        Debug.Log("Hello " + col.gameObject.name);
+                        LeanTween.move(_test, col.transform, 1f).setEase(_leanTweenType).setOnUpdate((float f) => { "Wow".Log(); }) ;
+                    }
+                    else
+                    {
+                        "This is my troop".Log(this);
+                    }
+                    
+                }
+            }
+
+        }
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position, _attackingRange);
         }
     }
 }
