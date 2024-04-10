43dfa535fa4916a9379ee8de528ac7bb276b4d96
fallenblood7080 <67326508+fallenblood7080@users.noreply.github.com>
2024-04-10 23:33:05 +0530

    Added Audio Base

--- Assets/Scripts/Core/AudioManager.cs
+++ Assets/Scripts/Core/AudioManager.cs
@@ -7,9 +7,14 @@ namespace Core
 {
     public class AudioManager : PersistentMonoSingleton<AudioManager>
     {
+        public bool CanPlayBackgroundMusic { get; private set; }
+        public bool CanPlaySfx { get; private set; }
+
         protected override void Awake()
         {
             base.Awake();
+
+            //Load Audio Settings for SaveFiles
         }
     }
 }
