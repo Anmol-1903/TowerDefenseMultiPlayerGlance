168df24cf7d16fb12d4d4adf29285d92893fff12
NamanSemalti <namansemalti@gmail.com>
2024-05-01 20:05:02 +0530

    Player Control assign

--- Assets/Scripts/Networking/Manager/PlayerManager.cs
+++ Assets/Scripts/Networking/Manager/PlayerManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Core;
+using Core.Input;
 using Networking;
 using Photon.Pun;
 using Tower;
@@ -9,6 +10,7 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager Instance;
+    [SerializeField] private InputManager[] inputManagers;
 
     private void Awake()
     {
@@ -20,17 +22,19 @@ public class PlayerManager : MonoBehaviour
 
     private void Start()
     {
-        Spawn();
+        GameManager.Instance.OnGameStart += Spawn;
     }
-
+    private void OnDestroy()
+    {
+        GameManager.Instance.OnGameStart -= Spawn;
+    }
     private void Spawn()
     {
         for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
         {
-            GameObject player = PhotonNetwork.Instantiate(NetworkManager.Instance.PlayerPrefab.name, playerSpawnPositions[i].position, Quaternion.identity);
-            // Assign different colors and enums to players
-            PlayerDataContainer playerController = player.GetComponent<PlayerDataContainer>();
-            playerController.SetPlayerProperties(GameManager.Instance.GameSettings.GetPlayerColors[i], i); // Assuming you have a PlayerController script
+            inputManagers[i].SetProperties(PhotonNetwork.PlayerList[i].ActorNumber);
+            // PlayerDataContainer playerController = player.GetComponent<PlayerDataContainer>();
+            // playerController.SetPlayerProperties(GameManager.Instance.GameSettings.GetPlayerColors[i], i); // Assuming you have a PlayerController script
         }
     }
 }
