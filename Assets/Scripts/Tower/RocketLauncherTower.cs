1ea192c51f914802d4ed9a23fed154fa210c2caa
fallenblood7080 <67326508+fallenblood7080@users.noreply.github.com>
2024-05-23 14:20:03 +0530

    Calculate Rocket Points

--- Assets/Scripts/Tower/RocketLauncherTower.cs
+++ Assets/Scripts/Tower/RocketLauncherTower.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 using System.Collections;
 using TowerType = Core.GameEnums.TowerType;
 using Troop;
+using System.Collections.Generic;
+using System.Net;
 
 namespace Tower
 {
     public class RocketLauncherTower : TowerBase
     {
         public bool IsActivelyShooting { get; private set; }
+
+        [SerializeField] private Transform startingPoint;
+        [SerializeField] private Transform target;
+        [SerializeField] private AnimationCurve curve;
+
+        private List<Vector3> points;
 
         protected override void Awake()
         {
@@ -17,13 +25,49 @@ namespace Tower
             UsedPaths = MaxPaths = 0;
         }
 
+        protected override void Start()
+        {
+            base.Start();
+            GenerateParabolicPoints(startingPoint.position, target.position, 16);
+            for (int i = 0; i < points.Count; i++)
+            {
+                if (i == points.Count - 1)
+                    break;
+
+                Debug.DrawLine(points[i], points[i + 1], Color.red, 10f);
+            }
+        }
+
+        private void GenerateParabolicPoints(Vector3 start, Vector3 end, int numberOfPoints)
+        {
+            Vector3 midpoint = (start + end) / 2f;
+
+            Vector3 vertex = new Vector3(midpoint.x, Mathf.Max(start.y, end.y) + 2f, midpoint.z);
+            for (int i = 0; i <= numberOfPoints; i++)
+            {
+                float t = i / (float)numberOfPoints;
+                Vector3 parabolicPoint = CalculateParabolicPoint(start, vertex, end, t);
+                points.Add(parabolicPoint);
+            }
+        }
+
+        private Vector3 CalculateParabolicPoint(Vector3 start, Vector3 vertex, Vector3 end, float t)
+        {
+            float oneMinusT = 1f - t;
+            Vector3 point = oneMinusT * oneMinusT * start + 2f * oneMinusT * t * vertex + t * t * end;
+            return point;
+        }
+
         public void Retarget(Vector3 pos)
         {
         }
 
         protected override void Spawn()
         {
-            throw new System.NotImplementedException();
+        }
+
+        protected override void Update()
+        {
         }
     }
 }
